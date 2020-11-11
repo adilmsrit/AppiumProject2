c1e0daec014659be023ba57dd4977aa5ca060d72
Mohammed Adil <adilmsrit@gmail.com>
2020-11-11 17:24:40 +0400

    Completed TC_4

--- src/eCommerce/ecommerce_tc_4.java
+++ src/eCommerce/ecommerce_tc_4.java
@@ -1,14 +1,13 @@
 package eCommerce;
 
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+import java.util.List;
 
 public class ecommerce_tc_4 extends baseForeCommerce {
 /* Test Case automated below
@@ -16,7 +15,7 @@ public class ecommerce_tc_4 extends baseForeCommerce {
 *
 * */
     @Test
-    public void validateOrdeinCheckoutPage() throws MalformedURLException {
+    public void validateOrdeinCheckoutPage() throws MalformedURLException, InterruptedException {
         AndroidDriver<AndroidElement> driver = capabilities();
         driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Helllooo");
         driver.findElementByXPath("//*[@text='Female']").click();
@@ -26,9 +25,26 @@ public class ecommerce_tc_4 extends baseForeCommerce {
                 .click();
         driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
 
-//        WebElement AddToCartElements = driver.findElements(By.xpath("//*[@text='']"));
+        List<AndroidElement> AddToCartElements = driver.findElements(By.xpath("//*[@text='ADD TO CART']"));
+
+        for (AndroidElement element : AddToCartElements) {
+            element.click();
+        }
 
         driver.findElementById("com.androidsample.generalstore:id/appbar_btn_cart").click();
+
+        Thread.sleep(2000);
+        List<AndroidElement> amountOfItemsInCart = driver.findElementsById("com.androidsample.generalstore:id/productPrice");
+        System.out.println(amountOfItemsInCart.size());
+
+        Double total = 0.00;
+        for (AndroidElement items : amountOfItemsInCart) {
+            total = total + Double.parseDouble(items.getText().substring(1));
+        }
+
+        double totalFromApp = Double.parseDouble(driver.findElementById("com.androidsample.generalstore:id/totalAmountLbl").getText().substring(1));
+
+        Assert.assertEquals(total, totalFromApp);
 
     }
 }
