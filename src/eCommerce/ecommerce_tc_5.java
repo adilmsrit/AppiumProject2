eec833b88d986bd8779e0cd38b8454f8c1e90274
Mohammed Adil <adilmsrit@gmail.com>
2020-11-12 14:10:58 +0400

    TestCase5

--- src/eCommerce/ecommerce_tc_5.java
+++ src/eCommerce/ecommerce_tc_5.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.util.List;
 
-public class ecommerce_tc_4 extends baseForeCommerce {
+public class ecommerce_tc_5 extends baseForeCommerce {
 /* Test Case automated below
 * Validate the total Amount displayed in the checkout page matches with the sum of products selected for shopping.
 *
@@ -18,15 +18,9 @@ public class ecommerce_tc_4 extends baseForeCommerce {
     public void validateOrdeinCheckoutPage() throws MalformedURLException, InterruptedException {
         AndroidDriver<AndroidElement> driver = capabilities();
         driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Helllooo");
-        driver.findElementByXPath("//*[@text='Female']").click();
-        driver.hideKeyboard();
-        driver.findElementById("android:id/text1").click();
-        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));")
-                .click();
         driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
 
         List<AndroidElement> AddToCartElements = driver.findElements(By.xpath("//*[@text='ADD TO CART']"));
-
         for (AndroidElement element : AddToCartElements) {
             element.click();
         }
@@ -46,19 +40,5 @@ public class ecommerce_tc_4 extends baseForeCommerce {
         Assert.assertEquals(total, totalFromApp);
     }
 
-    public int[] front11(int[] a, int[] b) {
-
-        int[] result = new int[2];
-
-        if (a.length == 0) result[0] = b[0];
-        else if (b.length == 0) result[0] =  a[0];
-        else {
-            result[0] = a[0];
-            result[1] = b[0];
-        }
-
-        return result;
-
-    }
 
 }
